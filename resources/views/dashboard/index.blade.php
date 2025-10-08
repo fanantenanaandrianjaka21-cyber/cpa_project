053e9e377cf36ae9ba17e8c2b77c211825168329
Andrianjaka Fanantenana <fanantenanaandrianjaka21@gmail.com>
2025-10-08 10:39:09 +0300

    tache: test ajout affichage du dashboard en utilisant git

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+<div class="container mt-5">
+    <h1>Dashboard</h1>
+    
+    <div class="row mb-4">
+        <div class="col-md-4">
+            <div class="card text-white bg-success mb-3">
+                <div class="card-header">Initiation Git</div>
+                <div class="card-body">
+                    <h5 class="card-title">mes taches sur Github</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <h3>Test git</h3>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Titre</th>
+                <th>Statut</th>
+            </tr>
+        </thead>
+        <tbody>
+ 
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
