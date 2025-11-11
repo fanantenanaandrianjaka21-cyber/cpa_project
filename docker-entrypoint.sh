56bd55c543df17aad8456c1a593eda583ed7ffa0
Mihossiraman <156893990+Tefymahery@users.noreply.github.com>
2025-11-11 12:26:17 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -e
 
+# Copier .env.example en .env si nécessaire
+if [ ! -f /var/www/html/.env ]; then
+    cp /var/www/html/.env.example /var/www/html/.env
+fi
+
 # Attendre que PostgreSQL soit prêt
 echo "Waiting for PostgreSQL..."
 until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USERNAME"; do
   sleep 2
 done
 
-# Générer la clé si nécessaire
+# Générer la clé Laravel si nécessaire
 php artisan key:generate --force
 
-# Lancer les migrations si besoin
+# Lancer les migrations
 php artisan migrate --force
 
 # Lancer le serveur Laravel
