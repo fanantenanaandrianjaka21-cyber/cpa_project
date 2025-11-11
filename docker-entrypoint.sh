86830f84a8e9548903123678a396772014226477
Mihossiraman <156893990+Tefymahery@users.noreply.github.com>
2025-11-11 15:11:52 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,8 +15,16 @@ done
 # Générer la clé Laravel si nécessaire
 php artisan key:generate --force
 
+# Découvrir les packages Laravel (après la DB disponible)
+php artisan package:discover --ansi
+
 # Lancer les migrations
 php artisan migrate --force
+
+# Générer les caches Laravel (optionnel mais recommandé)
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
 # Lancer le serveur Laravel
 php artisan serve --host=0.0.0.0 --port=8000
