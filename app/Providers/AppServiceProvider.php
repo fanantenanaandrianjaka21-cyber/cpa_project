04d1f1a0bd592a7a92064900f87c30b2b4139224
fanantenanaandrianjaka21-cyber <fanantenanaandrianjaka21@gmail.com>
2025-11-13 09:11:33 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (config('app.env') === 'production') {
+        /*if (config('app.env') === 'production') {
             URL::forceScheme('https');
-        }
+        }*/
     }
 }
 
