ffcab62e63f5e2f2d059dfcd491bcf70aa024b19
Andrianjaka Fanantenana <fanantenanaandrianjaka21@gmail.com>
2025-11-14 10:01:50 +0300

    mise a jour develop

--- config/mail.php
+++ config/mail.php
@@ -41,7 +41,7 @@ return [
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => 1000,
+            'timeout' => null,
             'auth_mode' => null,
         ],
 
